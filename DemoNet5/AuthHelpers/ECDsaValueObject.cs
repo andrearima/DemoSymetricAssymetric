9f38d9a0fe6b3944ab4935327e31d8e45eb29c44
André Arima <a>
2021-12-09 11:01:41 -0300

    Add project files.

---
+++ DemoNet5/AuthHelpers/ECDsaValueObject.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Security.Cryptography;
+
+namespace DemoNet5.AuthHelpers
+{
+    public static class ECDsaValueObject
+    {
+        private readonly static ECCurve DefaultCurve = ECCurve.NamedCurves.nistP256;
+        private static ECDsaSecurityKey _ecdssaKey;
+        public static ECDsaSecurityKey ObterECDsa()
+        {
+            if (_ecdssaKey != null) return _ecdssaKey;
+
+            var ecdsa = new ECDsaSecurityKey(ECDsa.Create(DefaultCurve))
+            {
+                KeyId = Guid.NewGuid().ToString()
+            };
+
+            _ecdssaKey = ecdsa;
+            return ecdsa;
+        }
+    }
+}
